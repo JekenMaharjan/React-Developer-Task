2c0eee7307fa4d9a0629af506640ad83cefa2646
JekenMaharjan <maharjanjeken@gmail.com>
2025-12-27 15:43:27 +0545

    setup css files with usable tailwindcss

--- task-tracker/src/App.jsx
+++ task-tracker/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './app.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div className='bg-red-700'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-blue-200 h-screen'>
+        <div className='bg-blue-400 text-white p-4 text-center font-bold text-3xl'>
+            React Task Tracker App
+        </div>
+
+        <div className='p-4'>
+            {/* App content goes here */}
+            
+        </div>
+    </div>
   )
 }
 
